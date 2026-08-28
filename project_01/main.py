abdba0849aa04020f74502479ff72b94871e27a2
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-28 22:18:54 +0330

    Loading the bg image

--- project_01/main.py
+++ project_01/main.py
@@ -7,6 +7,8 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Game")
 
+BG = pygame.image.load("space.png")
+
 def main():
     run = True
 
