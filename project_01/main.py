38a1c72f3f2948937f10bdfc1d9c4bbe932f64b8
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-28 22:23:15 +0330

    Fixing the relative pathing of the bg image

--- project_01/main.py
+++ project_01/main.py
@@ -7,7 +7,7 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Game")
 
-BG = pygame.image.load("./space.png")
+BG = pygame.image.load("./project_01/space.png")
 
 def draw():
     WIN.blit(BG, (0, 0))
