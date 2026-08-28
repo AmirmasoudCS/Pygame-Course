afca107f796cd23d040158d41714766cf1ad968b
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-28 22:39:06 +0330

    Adding paranthesis to the conditions of the key presses

--- project_01/main.py
+++ project_01/main.py
@@ -39,10 +39,10 @@ def main():
 
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT] or keys[pygame.K_a] and player.x - PLAYER_VEL >= 0:
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and player.x - PLAYER_VEL >= 0:
             player.x -= PLAYER_VEL
 
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d] and player.x + PLAYER_VEL + player.width <= WIDTH:
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
         
         draw(player)
