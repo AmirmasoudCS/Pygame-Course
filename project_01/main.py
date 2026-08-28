77b28439226e4d1275bde0560590e893f4a573c9
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-28 22:13:36 +0330

    Fixing the declaration of the setting mdoe function

--- project_01/main.py
+++ project_01/main.py
@@ -4,7 +4,7 @@ import random
 
 WIDTH, HEIGHT = 1000, 800
 
-WIN = pygame.display.set_mode(WIDTH, HEIGHT)
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Game")
 
 
