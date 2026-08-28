dbda5c95ca073b4c34500d3893f49fda6c6778e1
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-28 22:49:28 +0330

    Generating stars randomly

--- project_01/main.py
+++ project_01/main.py
@@ -13,6 +13,9 @@ BG = pygame.transform.scale(pygame.image.load("./project_01/space.png"), (WIDTH,
 PLAYER_WIDTH = 40
 PLAYER_HEGIHT = 60
 
+STAR_WIDTH = 10
+STAR_HEIGHT = 20
+
 PLAYER_VEL = 5
 
 FONT = pygame.font.SysFont("cosmicsans", 30)
@@ -39,11 +42,24 @@ def main():
     start_time = time.time()
     elapsed_time = 0
 
+    star_add_increment = 2000
+    star_count = 0
+
+    stars = []
+
     while run:
 
-        clock.tick(60)
+        star_count += clock.tick(60)
 
         elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+                
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
