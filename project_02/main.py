df5dd58b4dc72cd312f88af5b8de409cde798638
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-30 18:56:01 +0330

    Drawing rectangles around the ships in the main loop of our game

--- project_02/main.py
+++ project_02/main.py
@@ -14,17 +14,25 @@ SHIPS_HEIGT = 40
 
 YELLOW_SPACESHIP = pygame.image.load("./project_02/assets/spaceship_yellow.png")
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP, (SHIPS_WIDTH, SHIPS_HEIGT)), 90)
+YELLOW_SPACESHIP_X = WIDTH/4
+YELLOW_SPACESHIP_Y = HEIGHT/2
+
 
 RED_SPACESHIP = pygame.image.load("./project_02/assets/spaceship_red.png")
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP, (SHIPS_WIDTH, SHIPS_HEIGT)), 270)
+RED_SPACESHIP_X = WIDTH-WIDTH/4
+RED_SPACESHIP_Y = HEIGHT/2
 
 def draw():
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP, (WIDTH/4, HEIGHT/2))
-    WIN.blit(RED_SPACESHIP, (WIDTH-WIDTH/4, HEIGHT/2))
+    WIN.blit(YELLOW_SPACESHIP, (YELLOW_SPACESHIP_X, YELLOW_SPACESHIP_Y))
+    WIN.blit(RED_SPACESHIP, (RED_SPACESHIP_X, RED_SPACESHIP_Y))
     pygame.display.update()
 
 def main():
+
+    yellow = pygame.Rect(YELLOW_SPACESHIP_X, YELLOW_SPACESHIP_Y, SHIPS_WIDTH, SHIPS_HEIGT)
+    red = pygame.Rect(RED_SPACESHIP_X, RED_SPACESHIP_Y, SHIPS_WIDTH, SHIPS_HEIGT)
 
     clock = pygame.time.Clock()
 
