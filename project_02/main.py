22d753cec676149993c0e80e0a0d99d94e29d5ad
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-30 18:41:49 +0330

    Adding 60 fps to our main loop

--- project_02/main.py
+++ project_02/main.py
@@ -4,6 +4,8 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Battle Ship")
 
+FPS = 60
+
 WHITE = (255, 255, 255)
 
 def draw():
@@ -12,9 +14,11 @@ def draw():
 
 def main():
 
+    clock = pygame.time.Clock()
+
     run = True
     while run:
-
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
