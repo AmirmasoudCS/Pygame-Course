a497a95bafd85ac3c268ee43314121c54e25d84e
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-30 21:03:55 +0330

    Loading the bullets

--- project_03/main.py
+++ project_03/main.py
@@ -5,8 +5,19 @@ import time
 
 # Load images
 
+## Load Enemy Ships
+
 RED_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_red_small.png")
+RED_BULLET = pygame.image.load("./project_03/assets/pixel_laser_red.png")
+
 BLUE_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_blue_small.png")
+BLUE_BULLET = pygame.image.load("./project_03/assets/pixel_laser_blue.png")
+
 GREEN_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_green_small.png")
+GREEN_BULLET = pygame.image.load("./project_03/assets/pixel_laser_green.png")
+
+
+## Load Player Ship
 
 YELLOW_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_yellow.png")
+YELLOW_BULLET = pygame.image.load("./project_03/assets/pixel_laser_yellow.png")
