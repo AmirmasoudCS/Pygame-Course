824d79f82923da6ac35209fd7421cc6d8499a27c
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-30 21:01:27 +0330

    Loading the ships

---
+++ project_03/main.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+import time
+import time
+
+# Load images
+
+RED_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_red_small.png")
+BLUE_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_blue_small.png")
+GREEN_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_green_small.png")
+
+YELLOW_SPACE_SHIP = pygame.image.load("./project_03/assets/pixel_ship_yellow.png")
