a126f3d5f17e5b5101603f4b6b92cf07e8056567
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 20:04:29 +0330

    Implementing the function to rotate our image

---
+++ project_04/utils.py
@@ -0,0 +1,5 @@
+import pygame
+
+def blit_rotate_center(window, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
