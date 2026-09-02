4dec23c6f05e0c26769e47005a5deecfa1be0bdb
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 19:24:58 +0330

    Setting up the clock for our main loop to 60 fps

--- project_04/main.py
+++ project_04/main.py
@@ -15,11 +15,16 @@ HEIGHT = TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game")
 
+FPS = 60
+
 def main():
 
+    clock = pygame.time.Clock()
 
     run = True
     while run:
+
+        clock.tick(FPS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
