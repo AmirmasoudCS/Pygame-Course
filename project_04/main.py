5106bc30ac0066f4f1bc621d7371163ab52a8c18
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 19:07:04 +0330

    Loading the assets in

---
+++ project_04/main.py
@@ -0,0 +1,11 @@
+import pygame
+import time
+import math
+
+GRASS = pygame.image.load("assets/imgs/grass.jpg")
+TRACK = pygame.image.load("assets/imgs/track.png")
+BORDER = pygame.image.load("assets/imgs/track-border.png")
+FINISH = pygame.image.load("assets/imgs/finish.png")
+RED_CAR = pygame.image.load("assets/imgs/red-car.png")
+PURPLE_CAR = pygame.image.load("assets/imgs/purple-car.png")
+
