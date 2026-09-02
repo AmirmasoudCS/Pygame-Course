f850a44fff4c8560a1e65eb03d4e10a814caf558
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 19:22:03 +0330

    Fixing the title caption

--- project_04/main.py
+++ project_04/main.py
@@ -13,5 +13,5 @@ WIDTH = TRACK.get_width()
 HEIGHT = TRACK.get_height()
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-WIN.set_caption("Racing Game")
+pygame.display.set_caption("Racing Game")
 
