d3ad5dcc20ed26217db0203b74fc64f3a9dcbc9e
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 19:20:36 +0330

    Setting up the window

--- project_04/main.py
+++ project_04/main.py
@@ -9,3 +9,7 @@ FINISH = pygame.image.load("assets/imgs/finish.png")
 RED_CAR = pygame.image.load("assets/imgs/red-car.png")
 PURPLE_CAR = pygame.image.load("assets/imgs/purple-car.png")
 
+WIDTH = TRACK.get_width()
+HEIGHT = TRACK.get_height()
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
