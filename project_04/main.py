95f835eedf2f33e71d198091b36afd2d5a8bd190
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 20:10:44 +0330

    Trimming down the player car class a bit

--- project_04/main.py
+++ project_04/main.py
@@ -60,11 +60,7 @@ class AbstractCar:
 class PlayerCar(AbstractCar):
 
     IMG = PURPLE_CAR
-
-    def __init__(self, max_vel, rotation_vel):
-        super().__init__(max_vel, rotation_vel)
-        self.img = self.IMG
-
+    
 
 def draw(window, images):
 
