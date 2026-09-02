ad2315ab39b6383bda0a65bfa71941486d7828ae
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 19:29:03 +0330

    Fixing the relevant pathing to our assets

--- project_04/main.py
+++ project_04/main.py
@@ -2,12 +2,12 @@ import pygame
 import time
 import math
 
-GRASS = pygame.image.load("./assets/imgs/grass.jpg")
-TRACK = pygame.image.load("./assets/imgs/track.png")
-BORDER = pygame.image.load("./assets/imgs/track-border.png")
-FINISH = pygame.image.load("./assets/imgs/finish.png")
-RED_CAR = pygame.image.load("./assets/imgs/red-car.png")
-PURPLE_CAR = pygame.image.load("./assets/imgs/purple-car.png")
+GRASS = pygame.image.load("./project_04/assets/imgs/grass.jpg")
+TRACK = pygame.image.load("./project_04/assets/imgs/track.png")
+BORDER = pygame.image.load("./project_04/assets/imgs/track-border.png")
+FINISH = pygame.image.load("./project_04/assets/imgs/finish.png")
+RED_CAR = pygame.image.load("./project_04/assets/imgs/red-car.png")
+PURPLE_CAR = pygame.image.load("./project_04/assets/imgs/purple-car.png")
 
 WIDTH = TRACK.get_width()
 HEIGHT = TRACK.get_height()
