8b169cd225da22c975296708df426ae8fa86267d
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-02 20:05:32 +0330

    Blitting the image in the rotation function

--- project_04/utils.py
+++ project_04/utils.py
@@ -3,3 +3,4 @@ import pygame
 def blit_rotate_center(window, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
+    window.blit(rotated_image, new_rect.topleft)
