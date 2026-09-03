040d800cf8a305a0c89656181520b50f8254b8da
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-03 21:40:14 +0330

    Implementing a move functionality for our moving of the pieces on the board

--- project_05/checkers/board.py
+++ project_05/checkers/board.py
@@ -24,6 +24,17 @@ class Board:
             for col in range(row % 2, ROWS, 2):
                 pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+    def move(self, piece, row, col):
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
+        piece.move(row, col)
+
+        if row == ROWS or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                white_kings += 1
+            elif piece.color == RED:
+                red_kings += 1
+
     def create_board(self):
         for row in range(ROWS):
             self.board.append([])
