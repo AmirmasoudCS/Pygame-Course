4a72d766c547e4dabf262389d9d72e84ad82b417
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-03 20:32:44 +0330

    Implementing a function to draw the board of the checkers

--- project_05/checkers/board.py
+++ project_05/checkers/board.py
@@ -1,4 +1,10 @@
 import pygame
+from project_05.checkers.constants import (
+    BLACK,
+    ROWS,
+    RED,
+    SQUARE_SIZE,
+)
 
 class Board:
 
@@ -7,3 +13,11 @@ class Board:
         self.selected_piece = None
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
+
+    def draw_cubes(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    
