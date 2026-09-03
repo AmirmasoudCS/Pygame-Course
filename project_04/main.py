2745347cd227942cd55f140b64cbade04822fa43
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-03 08:50:42 +0330

    Implementing the move backward in the main game loop

--- project_04/main.py
+++ project_04/main.py
@@ -123,6 +123,9 @@ def main():
         if keys[pygame.K_w]:
             moved = True
             player_car.move_forward()
+        if keys[pygame.K_s]:
+            moved = True
+            player_car.move_backward()
 
         if not moved:
             player_car.reduce_speed()
