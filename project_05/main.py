f42138342439be4bad11b1f65fc6572f59aae65f
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-03 20:20:59 +0330

    Implementing the main function with the game loop running in it

--- project_05/main.py
+++ project_05/main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from project_05.checkers.constants import (
     WIDTH,
     HEIGHT,
@@ -8,10 +9,20 @@ from project_05.checkers.constants import (
     BLACK,
     BLUE,
     RED,
+    FPS,
 )
 
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
 
+def main():
 
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+
+        clock.tick(FPS)
+        pass
+    
