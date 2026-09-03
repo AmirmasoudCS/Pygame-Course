b32a9613536f3f777cad1d873edc34f14044aaed
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-03 08:55:54 +0330

    Removing the reduced_speed method from the abstract class and moving it to the playercar class since later we don't want our computer car to be able to reduce its speed

--- project_04/main.py
+++ project_04/main.py
@@ -72,10 +72,6 @@ class AbstractCar:
         self.y -= vertical
         self.x -= horizontal
 
-    def reduce_speed(self):
-        self.vel = max(self.vel - self.acceleration/2, 0)
-        self.move()
-
     def draw(self, window):
         blit_rotate_center(window, self.img, (self.x, self.y), self.angle)
 
@@ -83,7 +79,10 @@ class PlayerCar(AbstractCar):
 
     START_POS = (190, 200)
     IMG = PURPLE_CAR
-    
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration/2, 0)
+        self.move()
 
 def draw(window, images, player_car):
 
