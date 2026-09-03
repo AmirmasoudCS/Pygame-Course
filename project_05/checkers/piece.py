68ab29585c08e49530e821dc9af6d03e18020c10
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-03 20:48:49 +0330

    Defining a method to calculate the position of the pieces

--- project_05/checkers/piece.py
+++ project_05/checkers/piece.py
@@ -1,4 +1,4 @@
-from project_05.checkers.constants import RED, WHITE
+from project_05.checkers.constants import WHITE, SQUARE_SIZE
 
 class Piece:
     def __init__(self, row, col, color):
@@ -9,3 +9,9 @@ class Piece:
         self.direction = 1 if self.color == WHITE else -1
         self.x = 0
         self.y = 0
+        self.calc_pos()
+
+
+    def calc_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
