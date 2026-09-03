5d7f274041c939ff15cd55a5936826eacc7cb4df
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-03 08:42:13 +0330

    Updating our move function to actually follow physics trajectory

--- project_04/main.py
+++ project_04/main.py
@@ -61,7 +61,12 @@ class AbstractCar:
         self.move()
 
     def move(self):
-        self.x += self.vel
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
 
     def draw(self, window):
         blit_rotate_center(window, self.img, (self.x, self.y), self.angle)
