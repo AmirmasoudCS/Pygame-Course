fa35b8574638c75f47c83c1e2361488211e2a35b
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-04 18:13:12 +0330

    Adding a draw method to our paddle to actually draw them on the screen

--- project_06/main.py
+++ project_06/main.py
@@ -11,11 +11,15 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
 class Paddle:
+    COLOR = WHITE = (255, 255, 255)
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
 def draw(win):
     win.blit(BLACK)
