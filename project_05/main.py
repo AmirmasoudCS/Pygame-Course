ae2f9fc14f93b4b98c48f3e099605b061114cb55
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-04 13:08:24 +0330

    Updating the main loop of the game so we only calculate the ai move when it is actually its move

--- project_05/main.py
+++ project_05/main.py
@@ -35,18 +35,18 @@ def main():
 
         clock.tick(FPS)
 
+        if winner is None and game.turn == WHITE:
+            value, new_board = minimax(game.get_board(), 3, WHITE, game)
+            game.ai_move(new_board)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and game.turn == RED:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                if game.turn == RED:
-                    game.select(row, col)
-                else:
-                    value, new_board = minimax(game.get_board(), 3, WHITE, game)
-                    game.ai_move(new_board)
+                game.select(row, col)
 
         if winner is None:
             winner = game.winner()
