6ab540f9488083fcac39761e324546d62bae082c
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-04 10:18:17 +0330

    Adding a dark overlay and a font

--- project_05/checkers/constants.py
+++ project_05/checkers/constants.py
@@ -1,5 +1,7 @@
 import pygame
+pygame.font.init()
 
+FONT = pygame.font.SysFont("arial", 60, bold=True)
 FPS = 60
 
 WIDTH, HEIGHT = 800, 800
@@ -11,5 +13,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GRAY = (128, 128, 128)
+DARK_OVERLAY = (0, 0, 0, 150)
+
 
 CROWN = pygame.transform.scale(pygame.image.load("./project_05/assets/crown.png"), (44, 25))
