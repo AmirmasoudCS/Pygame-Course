ce56409af891d0d48d60d4d2ca8645418d8eaf45
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-04 11:01:14 +0330

    Loading the sounds we have

--- project_05/checkers/constants.py
+++ project_05/checkers/constants.py
@@ -17,3 +17,7 @@ DARK_OVERLAY = (0, 0, 0, 80)
 
 
 CROWN = pygame.transform.scale(pygame.image.load("./project_05/assets/crown.png"), (44, 25))
+
+BG_MUSIC = pygame.mixer.Sound("./project_05/assets/sounds/bg_music.mp3")
+MOVE_SOUND = pygame.mixer.Sound("./project_05/assets/sounds/move.wav")
+CAPTURE_SOUND = pygame.mixer.Sound("./project_05/assets/sounds/piece_capturing.wav")
