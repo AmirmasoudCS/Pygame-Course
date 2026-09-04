440bca6d0628eb1a787afe2f48788ff62692c714
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-04 09:19:57 +0330

    Debugging the way we access the current inside the board

--- project_05/checkers/board.py
+++ project_05/checkers/board.py
@@ -82,7 +82,7 @@ class Board:
             if left < 0:
                 break
 
-            current = self.board.get_piece(r, left)
+            current = self.board[r][left]
             if current == 0:
                 if skipped and not last:
                     break
@@ -114,7 +114,7 @@ class Board:
             if right >= COLS:
                 break
 
-            current = self.board.get_piece(r, right)
+            current = self.board[r][right]
             if current == 0:
                 if skipped and not last:
                     break
