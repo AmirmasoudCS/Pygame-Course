ca2aac3f685b5f4a237a93bc0d2818fcfaec788d
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-04 08:58:21 +0330

    Adding the skeleton for the get valid moves method inside the board

--- project_05/checkers/board.py
+++ project_05/checkers/board.py
@@ -58,3 +58,15 @@ class Board:
                 piece = self.board[row][col]
                 if piece != 0:
                     piece.draw(win)
+
+    def get_valid_moves(self, piece):
+        moves = {}
+        left = piece.col - 1
+        right = piece.col + 1
+        row = piece.row
+
+        if piece.color == RED or piece.king:
+            pass
+        
+        if piece.color == WHITE or piece.king:
+            pass
