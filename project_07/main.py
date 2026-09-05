537e2669f61edaca6aa004450ad51787c5bb6014
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 09:07:46 +0330

    Defining some constants

---
+++ project_07/main.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+FPS = 60
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 4, 4
+
+RECT_HEIGHT, RECT_WIDTH = HEIGHT // ROWS, WIDTH // 2
+
+OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BG_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
