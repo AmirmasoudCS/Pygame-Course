1d970b05f50ddc5191f0ffd3b2deebb4208f130e
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 14:11:34 +0330

    handling the movement for the right direction

--- project_07/main.py
+++ project_07/main.py
@@ -110,9 +110,16 @@ def move_tiles(window, tiles, clock, direction):
         move_check = lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
         ceil = True
 
-
     elif direction == "right":
-        pass
+        sort_function = lambda x: x.col
+        reversed = True
+        delta = (MOVE_VEL, 0)
+        boundary_check = lambda tile: tile.col == COLS - 1
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col+1}")
+        merge_check = lambda tile, next_tile: tile.x < next_tile.x - MOVE_VEL
+        move_check = lambda tile, next_tile: tile.x < next_tile.x - RECT_WIDTH - MOVE_VEL
+        ceil = False
+
     elif direction == "up":
         pass
     elif direction == "down":
