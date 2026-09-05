a8298a5ef4a95394e642a0e9eea8d00da81009a0
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 09:09:50 +0330

    Creating our main game loop

--- project_07/main.py
+++ project_07/main.py
@@ -18,3 +18,19 @@ FONT_COLOR = (119, 110, 101)
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
+
+def main():
+
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
