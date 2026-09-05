4a0765d5d842242e3a459c9194e9a3584dd41fa8
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 21:41:28 +0330

    Implementing a function to load blocks for the Block class

--- project_08/main.py
+++ project_08/main.py
@@ -114,7 +114,21 @@ class Object(pygame.sprite.Sprite):
 
     def draw(self, win):
         win.blit(self.image, (self.rect.x, self.rect.y))
-    
+
+def load_block(size):
+    path = join("project_08", "assets", "Terrain", "Terrain.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().init(x, y, size, size)
+        block = load_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
 
 def get_background(name):
     image = pygame.image.load(join("project_08", "assets", "Background", name))
