30935bd0d421d5ed27156881a41848e94dd62cde
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 10:27:53 +0330

    Writing the code for one of the directions to move the tiles

--- project_07/main.py
+++ project_07/main.py
@@ -90,6 +90,31 @@ def get_random_position(tiles):
             break
     return row, col
 
+def move_tiles(window, tiles, clock, direction):
+    updated = True
+    blocks = set()
+
+    if direction == "left":
+        sort_function = lambda x: x.col
+        reversed = False
+        delta = (-MOVE_VEL, 0)
+        boundary_check = lambda tile: tile.col == 0
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col-1}")
+        merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_VEL
+        move_check = lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
+        ceil = True
+
+
+    elif direction == "right":
+        pass
+    elif direction == "up":
+        pass
+    elif direction == "down":
+        pass
+
+    
+
+
 def generate_tiles():
     tiles = {}
     for _ in range(2):
