99cd6d39f4495dc2aaa4318e8ce5b77ddcdb80bb
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 20:03:43 +0330

    Implementing some internal methods for Player class to handle movement to left and right

--- project_08/main.py
+++ project_08/main.py
@@ -25,12 +25,24 @@ class Player(pygame.sprite.Sprite):
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
 
-    
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
 
 
 
