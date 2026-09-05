9d23f3c89272b68d2a1925f292d8e1d78540174a
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 21:45:21 +0330

    Fixing a typo

--- project_08/main.py
+++ project_08/main.py
@@ -107,7 +107,7 @@ class Object(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, name=None):
         super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
-        self.image = pygame.surface((width, height), pygame.SRCALPHA)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
         self.width = width
         self.height = height
         self.name = name
