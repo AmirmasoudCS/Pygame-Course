c7da2721946bebed6cd1c7c203d754298069f774
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-05 19:42:32 +0330

    Defining some of our global variable

---
+++ project_08/main.py
@@ -0,0 +1,20 @@
+import os
+import random
+import pygame
+import math
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+WHITE = (255, 255, 255)
+BG_COLOR = WHITE
+
+WIDTH, HEIGHT = 1000, 800
+
+FPS = 60
+
+PLAYER_VEL = 5
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
