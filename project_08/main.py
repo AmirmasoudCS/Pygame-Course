1f872635cd038a988920e76fb499acc4d21ea4fa
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-09-06 07:53:47 +0330

    Implementing a hit state for player

--- project_08/main.py
+++ project_08/main.py
@@ -61,6 +61,12 @@ class Player(pygame.sprite.Sprite):
         self.animation_count = 0
         self.fall_count = 0
         self.jump_count = 0
+        self.hit = False
+        self.hit_count 
+
+    def hit(self):
+        self.hit = True
+        self.hit_count = 0
 
     def jump(self):
         self.y_vel = -self.GRAVITY * 8
@@ -90,12 +96,19 @@ class Player(pygame.sprite.Sprite):
         self.y_vel += min(1, self.fall_count / fps * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
+        if self.hit:
+            self.hit_count += 1
+        if self.hit_count > fps*2:
+            self.hit = False
+
         self.fall_count += 1
         self.update_sprite()
 
     def update_sprite(self):
         sprite_sheet = "idle"
-        if self.y_vel < 0:
+        if self.hit:
+            sprite_sheet = "hit"
+        elif self.y_vel < 0:
             if self.jump_count == 1:
                 sprite_sheet = "jump"
             elif self.jump_count == 2:
